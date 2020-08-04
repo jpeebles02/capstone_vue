fdf4ac0eed1ffa4731966ea15f801771609cb2ea
Justin Peebles <justin.p.peebles@gmail.com>
2020-08-04 14:53:49 -0500

    converting to jpeeblesfitness

--- src/router.js
+++ src/router.js
@@ -18,35 +18,32 @@ import Appointments from "./views/Appointments.vue";
 import Meals from "./views/Meals.vue";
 import Schedules from "./views/Schedule.vue";
 import Carts from "./views/Carts.vue";
-import MapPage from "./views/MapPage.vue";
-
+import mappage from "./views/mappage.vue";
 
 Vue.use(Router);
 
 export default new Router({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes: [
-        { path: "/", name: "home", component: Home },
-        { path: "/signup", name: "signup", component: Signup },
-        { path: "/login", name: "login", component: Login },
-        { path: "/logout", name: "logout", component: Logout },
-        { path: "/userhomepage", name: "user-home-page", component: UserHomePage },
-        { path: "/exercises", name: "exercises-index", component: ExercisesIndex },
-        { path: "/exercises/:id", name: "exercises-show", component: ExercisesShow },
-        { path: "/userweights/new", name: "user-weights-new", component: UserWeightsNew },
-        { path: "/routines/new", name: "routines-new", component: RoutinesNew },
-        { path: "/appointments/new", name: "appointments-new", component: AppointmentsNew },
-        { path: "/routines/:id", name: "routines-show", component: RoutinesShow },
-        { path: "/routines", name: "routines", component: Routines },
-        { path: "/products", name: "products", component: Products },
-        { path: "/products/:id", name: "products-show", component: ProductsShow },
-        { path: "/appointments", name: "appointments", component: Appointments },
-        { path: "/meals", name: "meals", component: Meals },
-        { path: "/schedules", name: "schedule", component: Schedules },
-        { path: "/carts", name: "cart", component: Carts },
-        { path: "/mappage", name: "mappage", component: MapPage }
-    ]
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    { path: "/", name: "home", component: Home },
+    { path: "/signup", name: "signup", component: Signup },
+    { path: "/login", name: "login", component: Login },
+    { path: "/logout", name: "logout", component: Logout },
+    { path: "/userhomepage", name: "user-home-page", component: UserHomePage },
+    { path: "/exercises", name: "exercises-index", component: ExercisesIndex },
+    { path: "/exercises/:id", name: "exercises-show", component: ExercisesShow },
+    { path: "/userweights/new", name: "user-weights-new", component: UserWeightsNew },
+    { path: "/routines/new", name: "routines-new", component: RoutinesNew },
+    { path: "/appointments/new", name: "appointments-new", component: AppointmentsNew },
+    { path: "/routines/:id", name: "routines-show", component: RoutinesShow },
+    { path: "/routines", name: "routines", component: Routines },
+    { path: "/products", name: "products", component: Products },
+    { path: "/products/:id", name: "products-show", component: ProductsShow },
+    { path: "/appointments", name: "appointments", component: Appointments },
+    { path: "/meals", name: "meals", component: Meals },
+    { path: "/schedules", name: "schedule", component: Schedules },
+    { path: "/carts", name: "cart", component: Carts },
+    { path: "/mappage", name: "mappage", component: mappage }
+  ]
 });
-
-
